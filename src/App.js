ad2b998fa61e7e73174285eb2e4d222c1d5c220b
Anton <kornilov.anthon@yandex.ru>
2021-04-12 19:17:17 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {Recipe} from './pages/Recipe';
 function App() {
   return (
     <>
-      <Router>
+      <Router basename='/react-food'>
 
         <Header />
         <main className="container content">
